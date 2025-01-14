441e8fa1613384f040e02736bd430bf024b73b3a
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2025-01-14 21:25:24 +0900

    yaml to python

---
+++ scripts/yaml_to_json.py
@@ -0,0 +1,11 @@
+import yaml
+import json
+
+path = "tasks/backlog.yaml"
+# backlog.yamlを読み込む
+with open(path, "r") as file:
+    data = yaml.safe_load(file)
+
+path = path.replace(".yaml", ".json")
+with open(path, "w") as file:
+    json.dump(data, file, indent=2, ensure_ascii=False)
