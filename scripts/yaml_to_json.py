df9a16a92ddd0c2674fec01b3f4c489a572f5b47
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2025-01-16 05:13:02 +0900

    change path

--- scripts/yaml_to_json.py
+++ scripts/yaml_to_json.py
@@ -1,7 +1,8 @@
 import yaml
 import json
 
-path = "tasks/backlog.yaml"
+# path = "tasks/backlog.yaml"
+path = "ai_project_manager_data/tasks/backlog.yaml"
 # backlog.yamlを読み込む
 with open(path, "r") as file:
     data = yaml.safe_load(file)
