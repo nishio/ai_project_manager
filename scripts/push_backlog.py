6cc3b67a6601d580408940c77739f2beb878f19d
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2025-02-27 20:35:44 +0900

    rename

--- scripts/push_backlog.py
+++ scripts/push_backlog.py
@@ -12,13 +12,14 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 
+
 def update_or_clone_repo():
     # Get data repository path from environment variable
     repo_path = os.getenv("DATA_ROOT", "../ai_project_manager_data")
     os.chdir(repo_path)
     subprocess.run(["git", "status"], check=True)
     subprocess.run(["git", "add", "-u"], check=True)
-    subprocess.run(["git", "commit", "-m", "update"], check=True)
+    subprocess.run(["git", "commit", "-m", "update backlog"], check=True)
     subprocess.run(["git", "push"], check=True)
 
 
